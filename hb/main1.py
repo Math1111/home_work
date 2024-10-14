b667cd4ef1d298056c5c65e4cd5a93b4ed60733c
Math1111 <emelyanov2704@mail.ru>
2024-10-14 22:31:29 +0300

    +

--- hb/main1.py
+++ hb/main1.py
@@ -3,4 +3,10 @@ hb1=Hitbox(0,100,100,100)
 hb2=Hitbox(150,100,100,100)
 hb3=Hitbox(100,50,100,100)
 
-from tkinter import*
+print(f"верхняя граница hb1: {hb1.top}, верхняя граница hb2: {hb2.top}")
+print(f"нижняя граница hb1: {hb1.bottom}, нижняя граница hb2: {hb2.bottom}")
+print(f"левая граница hb1: {hb1.left}, левая граница hb2: {hb2.left}")
+print(f"равая граница hb1: {hb1.right}, правая граница hb2: {hb2.right}")
+
+intersection=hb1.intersects(hb2)
+print(intersection)
