1e1cd9e29da47ccae89293d60a73174f6be38355
Math1111 <emelyanov2704@mail.ru>
2024-10-14 22:42:50 +0300

    +

--- hb/main1.py
+++ hb/main1.py
@@ -1,12 +1,16 @@
 from hitbox import Hitbox
 hb1=Hitbox(0,100,100,100)
-hb2=Hitbox(150,100,100,100)
-hb3=Hitbox(100,50,100,100)
+hb2=Hitbox(50,80,100,100)
+hb3=Hitbox(140,100,100,100)
 
 print(f"верхняя граница hb1: {hb1.top}, верхняя граница hb2: {hb2.top}")
 print(f"нижняя граница hb1: {hb1.bottom}, нижняя граница hb2: {hb2.bottom}")
 print(f"левая граница hb1: {hb1.left}, левая граница hb2: {hb2.left}")
 print(f"равая граница hb1: {hb1.right}, правая граница hb2: {hb2.right}")
 
-intersection=hb1.intersects(hb2)
-print(intersection)
+intersection1 = hb1.intersects(hb2)
+intersection2 = hb1.intersects(hb3)
+intersection3 = hb2.intersects(hb3)
+
+print(f'Первый и второй: {intersection1}, первый и третий: {intersection2}, '
+      f'второй и третий: {intersection3}.')
---
+++ hb/main2.py
@@ -0,0 +1,16 @@
+from hitbox import Hitbox
+hb1=Hitbox(0,100,100,100)
+hb2=Hitbox(120,100,100,100)
+hb3=Hitbox(240,100,100,100)
+
+print(f"верхняя граница hb1: {hb1.top}, верхняя граница hb2: {hb2.top}")
+print(f"нижняя граница hb1: {hb1.bottom}, нижняя граница hb2: {hb2.bottom}")
+print(f"левая граница hb1: {hb1.left}, левая граница hb2: {hb2.left}")
+print(f"равая граница hb1: {hb1.right}, правая граница hb2: {hb2.right}")
+
+intersection1 = hb1.intersects(hb2)
+intersection2 = hb1.intersects(hb3)
+intersection3 = hb2.intersects(hb3)
+
+print(f'Первый и второй: {intersection1}, первый и третий: {intersection2}, '
+      f'второй и третий: {intersection3}.')
